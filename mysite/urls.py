7216c9aff79f6f598db3d711e0d039679e9419a2
sarthak <shrmsarthak2661@gmail.com>
2023-05-27 12:28:12 -0400

    css added

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
 urlpatterns = [
     path('', include('myapp.urls')),
     path('admin/', admin.site.urls),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
